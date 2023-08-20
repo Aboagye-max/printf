8133d665ff054005ff2c7df6ba5eb2bbb2b11650
Aboagye Maxwell <135050867+Aboagye-max@users.noreply.github.com>
2023-08-20 15:50:56 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -6,21 +6,26 @@ int _printf(const char *format, ...)
 {
     va_list args;
     va_start(args, format);
- 
+
     while (*format != '\0') {
-        if (*format == 's') {
-            char *str = va_arg(args, char*);
-            printf("%s\n", str);
-        } else if (*format == 'c') {
-            int c = va_arg(args, int);
-            printf("%c\n", c);
-        } else if (*format == 'd' || *format == 'i') {
-            int num = va_arg(args, int);
-            printf("%d\n", num);
+        if (*format == '%') {
+            format++; // Move past the '%'
+            if (*format == 's') {
+                char *str = va_arg(args, char*);
+                printf("%s\n", str);
+            } else if (*format == 'c') {
+                int c = va_arg(args, int); // char is promoted to int
+                printf("%c\n", c);
+            } else if (*format == 'd' || *format == 'i') {
+                int num = va_arg(args, int);
+                printf("%d\n", num);
+            }
+        } else {
+            putchar(*format); // Print non-format specifier characters
         }
-        ++format;
+        format++;
     }
- 
+
     va_end(args);
     return 0;
 }
