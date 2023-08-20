fbdaefcb9d5465965268ec78b52a20a274749f14
Aboagye Maxwell <135050867+Aboagye-max@users.noreply.github.com>
2023-08-20 16:16:26 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,26 +1,47 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include "main.h"
+
+/**
+ * _printf - prints output according to format
+ * @format: format string
+ * Return: number of characters printed, excluding \0
+ */
 
 int _printf(const char *format, ...)
 {
-    va_list args;
-    va_start(args, format);
- 
-    while (*format != '\0') {
-        if (*format == 's') {
-            char *str = va_arg(args, char*);
-            printf("%s\n", str);
-        } else if (*format == 'c') {
-            int c = va_arg(args, int);
-            printf("%c\n", c);
-        } else if (*format == 'd' || *format == 'i') {
-            int num = va_arg(args, int);
-            printf("%d\n", num);
-        }
-        ++format;
-    }
- 
-    va_end(args);
-    return 0;
+	va_list arg;
+	int i = 0, j = 0;
+	int nchar = 0;
+	milk_t milk[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"%", print_per},
+		{"d", print_di},
+		{"i", print_di},
+		{NULL, NULL}
+	};
+
+	va_start(arg, format);
+
+	while (format != NULL && format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			while (format[i] == ' ')
+				i++;
+			while (milk[j].id != NULL)
+			{
+				if (*milk[j].id == format[i])
+					nchar += milk[j].f(arg);
+				j++;
+			}
+			i++;
+		}
+		_putchar(format[i]);
+		nchar++;
+		i++;
+		j = 0;
+	}
+	va_end(arg);
+	return (nchar);
 }
