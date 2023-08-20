74d4b19b935bcf67c0b7bc973634524807b40ca8
Aboagye Maxwell <135050867+Aboagye-max@users.noreply.github.com>
2023-08-20 16:42:37 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -2,19 +2,6 @@
 #include <stdio.h>
 #include "main.h"
 
-
-int digit_count(int n)
-{
-	int count = 0;
-
-	while (n > 0 || n < 0)
-	{
-		count++;
-		n = n / 10;
-	}
-	return (count);
-}
-
 int _printf(const char *format, ...)
 {
     va_list args;
@@ -22,55 +9,14 @@ int _printf(const char *format, ...)
  
     while (*format != '\0') {
         if (*format == 's') {
-	    int i = 0;
-            char *s = va_arg(args, char *);
-	    while (s[i] != '\0')
-	    {
-		_putchar(s[i]);
-		i++;
-	    }
+            char *str = va_arg(args, char*);
+            printf("%s\n", str);
         } else if (*format == 'c') {
             int c = va_arg(args, int);
-            _putchar(c);
+            printf("%c\n", c);
         } else if (*format == 'd' || *format == 'i') {
-	        int i, k = 0, numlength;
-		int n = va_arg(arg, int);
-		unsigned int pos_n = 0;
-		int mul = 1;
-	
-		if (n == 0)
-		{
-			_putchar(pos_n + '0');
-			return (1);
-		}
-		else if (n < 0)
-		{
-			_putchar('-');
-			pos_n = -n;
-			k++;
-		}
-		else
-		{
-			pos_n = n;
-		}
-	
-		numlength = digit_count(pos_n);
-	
-		i = numlength;
-		while (i != 1)
-		{
-			mul = mul * 10;
-			i--;
-		}
-	
-		_putchar((pos_n / mul) + '0');
-		while (mul != 1)
-		{
-			mul = mul / 10;
-			_putchar(((pos_n / mul) % 10) + '0');
-		}
-	
-		k += numlength;
+            int num = va_arg(args, int);
+            printf("%d\n", num);
         }
         ++format;
     }
