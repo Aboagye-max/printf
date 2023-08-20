611134812e3218374f519f75131d930b8dbbc23f
Aboagye Maxwell <135050867+Aboagye-max@users.noreply.github.com>
2023-08-20 15:38:45 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,32 +1,47 @@
 #include "main.h"
 
 int _printf(const char *format, ...) {
-     va_list args;
-     va_start(args, format);
-  
-     while (*format != '\0') {
-         if (*format == '%') {
-             format++; // Move to the character after '%'
-             
-             if (*format == 's') {
-                 char *str = va_arg(args, char *);
-                 printf("%s", str);
-             } else if (*format == 'c') {
-                 int c = va_arg(args, int);
-                 printf("%c", c);
-             } else if (*format == 'd' || *format == 'i') {
-                 int d = va_arg(args, int);
-                 printf("%d", d);
-             } else {
-                 putchar('%');
-                 putchar(*format);
-             }
-         } else {
-             putchar(*format);
-         }
-         format++;
-     }
-  
-     va_end(args);
-     return 0;
- }
+    va_list args;
+    va_start(args, format);
+
+    int count = 0;
+    char c;
+    while ((c = *format++) != '\0') {
+        if (c == '%') {
+            c = *format++;
+            if (c == '\0') {
+                // Incomplete format specifier
+                break;
+            }
+            switch (c) {
+                case 'c':
+                    putchar(va_arg(args, int));
+                    count++;
+                    break;
+                case 's': {
+                    const char *str = va_arg(args, const char *);
+                    while (*str != '\0') {
+                        putchar(*str);
+                        str++;
+                        count++;
+                    }
+                    break;
+                }
+                case '%':
+                    putchar('%');
+                    count++;
+                    break;
+                default:
+                    putchar('%');
+                    putchar(c);
+                    count += 2;
+                    break;
+            }
+        } else {
+            putchar(c);
+            count++;
+        }
+    }
+    va_end(args);
+    return count;
+}
