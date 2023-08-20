9d432b954d5f73dc797094124caaeba8e4cf1679
Aboagye Maxwell <135050867+Aboagye-max@users.noreply.github.com>
2023-08-20 15:22:36 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,27 +1,32 @@
 #include "main.h"
 
-int _printf(const char *format, ...)
-{
-    va_list args;
-    va_start(args, format);
- 
-    while (*format != '\0') {
-        if (*format == '%s') {
-            int i = va_arg(args, int);
-            printf("%s\n", i);
-        } else if (*format == '%c') {
-            char c = va_arg(args, char);
-            printf("%c\n", c);
-        } else if (*format == '%d') {
-            int d = va_arg(args, int);
-            printf("%d\n", d);
-        } else if (*format == '%i'){
-            int i = va_arg(args, int);
-            printf("%i\n", d);
-        }
-        ++format;
-    }
- 
-    va_end(args);
-    return (0);
-}
+int _printf(const char *format, ...) {
+     va_list args;
+     va_start(args, format);
+  
+     while (*format != '\0') {
+         if (*format == '%') {
+             format++; // Move to the character after '%'
+             
+             if (*format == 's') {
+                 char *str = va_arg(args, char *);
+                 printf("%s", str);
+             } else if (*format == 'c') {
+                 int c = va_arg(args, int);
+                 printf("%c", c);
+             } else if (*format == 'd' || *format == 'i') {
+                 int d = va_arg(args, int);
+                 printf("%d", d);
+             } else {
+                 putchar('%');
+                 putchar(*format);
+             }
+         } else {
+             putchar(*format);
+         }
+         format++;
+     }
+  
+     va_end(args);
+     return 0;
+ }
