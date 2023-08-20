97508d39332c89e6080b5789b10044838f71d5cb
Aboagye Maxwell <135050867+Aboagye-max@users.noreply.github.com>
2023-08-20 18:50:57 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,56 +1,28 @@
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include "main.h"
 
-int _printf(const char *format, ...) {
+int _printf(const char *format, ...)
+{
     va_list args;
     va_start(args, format);
-
-    int char_count = 0;
-
+ 
     while (*format != '\0') {
-        if (*format == '%') {
-            format++; // Move past the '%'
-            char specifier = *format;
-
-            switch (specifier) {
-                case 'c':
-                    putchar(va_arg(args, int));
-                    char_count++;
-                    break;
-                case 's':
-                    char *str = va_arg(args, char *);
-                    while (*str != '\0') {
-                        putchar(*str);
-                        str++;
-                        char_count++;
-                    }
-                    break;
-                case '%':
-                    putchar('%');
-                    char_count++;
-                    break;
-                case 'd':
-                case 'i':
-                    // Handle integer printing
-                    // Not implemented in this simplified version
-                    break;
-                // Handle other cases for different specifiers
-                // ...
-                default:
-                    // Unsupported specifier, just print as is
-                    putchar('%');
-                    putchar(specifier);
-                    char_count += 2;
-                    break;
-            }
-        } else {
-            putchar(*format);
-            char_count++;
+        if (*format == 's') {
+            // Use char* instead of int for strings
+            char *s = va_arg(args, char*);
+            printf("%s\n", s);
+        } else if (*format == 'c') {
+            // Use int instead of char for characters
+            int c = va_arg(args, int);
+            printf("%c\n", c);
+        } else if (*format == 'd' || *format == 'i') {
+            int d = va_arg(args, int);
+            printf("%d\n", d);
         }
-
-        format++;
+        ++format;
     }
-
+ 
     va_end(args);
-    return char_count;
+    return 0; // Return a value other than 0 to indicate an error
 }
