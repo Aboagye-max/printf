70f921e4233260adfe4c9119b4884329dbc05716
Aboagye Maxwell <135050867+Aboagye-max@users.noreply.github.com>
2023-08-20 16:18:36 +0000

    Update main.h

--- main.h
+++ main.h
@@ -3,7 +3,18 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <unistd.h>
+
+typedef struct milk
+{
+	char *id;
+	int (*f)(va_list);
+} milk_t;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_c(va_list);
+int print_s(va_list);
+int print_per(va_list);
+int print_di(va_list);
 #endif
